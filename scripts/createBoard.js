4bacc874754b52f8ffc76f3023edcf7d797ce246
Magnolia-code <elly1r@mail.ru>
2024-11-03 16:09:02 +0300

    Update createBoard.js

--- scripts/createBoard.js
+++ scripts/createBoard.js
@@ -1,29 +1,38 @@
-import { startTimer } from "./timer.js";
-import { createIconsArray, createCard } from "./cards.js";
+import { startTimer } from './timer.js';
+import { createIconsArray, createCard } from './cards.js';
 
 export function createBoard(count, columns) {
-    const gameBoard = document.querySelector('.board');
-    gameBoard.textContent = "";
+  const gameBoard = document.querySelector(".board");
 
-    const template = document.querySelector('#gameTableTemplate').cloneNode(true).content;
-    const icons = createIconsArray(count);
-    const gameTable = template.querySelector('.table');
-    const restartBtn = template.querySelector('.table__button');
-    // for (let i = 0; i < count; i++) {
-    //     gameTable.append(createCard());
-    // }
-    icons.forEach((icon) => {
-        gameTable.append(createCard(icon));
-      });
-    gameTable.style = `
-      grid-template-columns: repeat(${columns}, 1fr);
-      grid-template-rows: repeat(${columns}, 1fr);
-    `;
-    gameBoard.append(gameTable);
-    restartBtn.addEventListener("click", () => {
-        location.reload();
-    });
-    gameBoard.append(restartBtn);
-    startTimer();
-}
+  gameBoard.textContent = "";
 
+  // Создание клона шаблона
+  const template = document.querySelector('#gameTableTemplate').cloneNode(true).content;
+  // В шаблоне находится таблица
+  const gameTable = template.querySelector('.table');
+  // В шаблоне находится кнопка "Рестарт"
+  const restartBtn = template.querySelector(".table__button");
+
+  // Создание определенного количества иконок
+  const icons = createIconsArray(count);
+
+  // Заполнение ячеек карточками
+  icons.forEach((icon) => {
+    gameTable.append(createCard(icon));
+  });
+
+  gameTable.style = `
+  grid-template-columns: repeat(${columns}, 1fr);
+  grid-template-rows: repeat(${columns}, 1fr);
+  `;
+
+  gameBoard.append(gameTable);
+
+  restartBtn.addEventListener("click", () => {
+    location.reload();
+  });
+
+  gameBoard.append(restartBtn);
+
+  startTimer();
+};
